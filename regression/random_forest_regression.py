6e9392145d1f7d4e5ff306255d543d806be36412
Zlatian Iliev <zlatian.iliev@oneclipboard.com>
2019-03-11 19:54:32 +0100

    Add an exmaple of a random forest regression

---
+++ regression/random_forest_regression.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
+
+# import dataset
+dataset = pd.read_csv('../data/Position_Salaries.csv')
+
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values.reshape(-1,1)
+
+# Fitting Random Forest Regression
+regressor = RandomForestRegressor(n_estimators = 300, random_state = 0)
+regressor.fit(X, y)
+
+# Visualising the Random Forest Regression results
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape(len(X_grid), 1)
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Salary According To Position using Random Forest Regression')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+y_pred=regressor.predict(np.array([[6.5]]))
+print('Salary prediction with Random Forest Regression for level 6.5: ', y_pred)
