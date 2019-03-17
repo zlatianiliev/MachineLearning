21a8046e815d868daa8bdeb27f13b9a7e69d41d2
Zlatian Iliev <zlatian.iliev@oneclipboard.com>
2019-03-17 18:12:03 +0100

    Add and example of a kernel principal component analysis, update lda and pca visualisation

--- dimensionality_reduction/kernel_pca.py
+++ dimensionality_reduction/kernel_pca.py
@@ -3,28 +3,28 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.decomposition import KernelPCA
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 from matplotlib.colors import ListedColormap
 
 # Importing the dataset
-dataset = pd.read_csv('../data/Wine.csv')
-X = dataset.iloc[:, 0:13].values
-y = dataset.iloc[:, 13].values
+dataset = pd.read_csv('../data/Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Applying LDA
-lda = LDA(n_components = 2)
-X_train = lda.fit_transform(X_train, y_train)
-X_test = lda.transform(X_test)
+# Applying Kernel PCA
+kpca = KernelPCA(n_components = 2, kernel = 'rbf')
+X_train = kpca.fit_transform(X_train)
+X_test = kpca.transform(X_test)
 
 # Fitting Logistic Regression to the Training set
 classifier = LogisticRegression(random_state = 0)
@@ -58,20 +58,21 @@ print('F1 Score: ', f1_score)
 
 
 def visualise(X_set, y_set, title):
-    X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
-                         np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
+    X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                         np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
     plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-                 alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue')))
+                 alpha = 0.75, cmap = ListedColormap(('red', 'green')))
     plt.xlim(X1.min(), X1.max())
     plt.ylim(X2.min(), X2.max())
     for i, j in enumerate(np.unique(y_set)):
         plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                    c=ListedColormap(('red', 'green', 'blue'))(i), label=j)
+                    c = ListedColormap(('red', 'green'))(i), label = j)
     plt.title(title)
-    plt.xlabel('LD1')
-    plt.ylabel('LD2')
+    plt.xlabel('PC1')
+    plt.ylabel('PC2')
     plt.legend()
     plt.show()
+
 
 # Visualising the Training set results
 X_set, y_set = X_train, y_train
--- dimensionality_reduction/principal_component_analysis.py
+++ dimensionality_reduction/principal_component_analysis.py
@@ -37,36 +37,48 @@ y_pred = classifier.predict(X_test)
 # Making the Confusion Matrix
 cm = confusion_matrix(y_test, y_pred)
 
+# Printing the Accuracy, Precision, Recall and F1 Score
+# Accuracy = (TP + TN) / (TP + TN + FP + FN)
+# Precision = TP / (TP + FP)
+# Recall = TP / (TP + FN)
+# F1 Score = 2 * Precision * Recall / (Precision + Recall)
+
+total = cm[0][0] + cm[0][1] + cm[1][0] + cm[1][1]
+correctly_classified = cm[0][0] + cm[1][1]
+accuracy = correctly_classified / total
+print('Accuracy: ', accuracy)
+
+precision = cm[0][0] / (cm[0][0] + cm[0][1])
+print('Precision: ', precision)
+
+recall = cm[0][0] / (cm[0][0] + cm[1][0])
+print('Recall: ', recall)
+
+f1_score = 2 * precision * recall / (precision + recall)
+print('F1 Score: ', f1_score)
+
+
+def visualise(X_set, y_set, title):
+    X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
+                         np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
+    plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+                 alpha=0.75, cmap=ListedColormap(('red', 'green', 'blue')))
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+    for i, j in enumerate(np.unique(y_set)):
+        plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                    c=ListedColormap(('red', 'green', 'blue'))(i), label=j)
+    plt.title(title)
+    plt.xlabel('PC1')
+    plt.ylabel('PC2')
+    plt.legend()
+    plt.show()
+
+
 # Visualising the Training set results
 X_set, y_set = X_train, y_train
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
-plt.title('Logistic Regression (Training set)')
-plt.xlabel('PC1')
-plt.ylabel('PC2')
-plt.legend()
-plt.show()
+visualise(X_set, y_set, 'Logistic Regression (Test set)')
 
 # Visualising the Test set results
 X_set, y_set = X_test, y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
-plt.title('Logistic Regression (Test set)')
-plt.xlabel('PC1')
-plt.ylabel('PC2')
-plt.legend()
-plt.show()
+visualise(X_set, y_set, 'Logistic Regression (Training set)')
